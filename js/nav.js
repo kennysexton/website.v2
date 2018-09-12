f4841dfdec3143fdb486eead5fecc36973d68904
kenny <kennysexton4@gmail.com>
2018-09-12 11:39:45 -0400

    nav bar update.  Mobile nav support added. Minor img file changes,  Add blank HTML pages

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+//Script for opening and closing menu
+
+function openNav() {
+  	document.getElementById("mySidebar").style.width = "250px";
+}
+function closeNav() {
+		document.getElementById("mySidebar").style.width = "0px";
+}	
