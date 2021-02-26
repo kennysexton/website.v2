11974b2cfa43e250a53c34da21a17ec4e644b291
Kenny Sexton <kennysexton4@gmail.com>
2021-02-26 12:49:27 -0500

    Aerial page improvements (#25)
    * adding a video
    
    * still need to fix 1px short video,  and create nav with calculatable height
    
    * readjusted navbar size
    
    * removing nav bar dependency on bootstrap
    
    * reusable nav back in action
    
    * improved nav swapping
    
    * a lot of small style changes.  all now useing nav replace
    
    * fixed pixel gap
    
    * auto format
    
    * adding google fonts swap parameter

--- js/htmlReuse.js
+++ js/htmlReuse.js
@@ -1,7 +1,10 @@
-$(function(){
-	$('#footer').load('htmlSegments/footer.html');
+$(function () {
+  $('#footer').load('htmlSegments/footer.html');
 });
 
-$(function(){
-	$('#navBar').load('htmlSegments/navBar.html');
+$(function () {
+  $('nav').load('htmlSegments/navBar.html', function () {
+    setActive(); // Add active class to current tab
+  });
+
 });
--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,18 @@
 //Script for opening and closing menu
-
 function openNav() {
-  	document.getElementById("mySidebar").style.width = "55%";
+  document.getElementById("mySidebar").style.width = "55%";
 }
 function closeNav() {
-		document.getElementById("mySidebar").removeAttribute("style");
+  document.getElementById("mySidebar").removeAttribute("style");
 }
 
-
-$(window).on('resize',function(){  // detects change in width
-        closeNav();
+$(window).on('resize', function () {  // detects change in width
+  closeNav();
 });
 
 
-$(function() { // Set Active
-  $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-});
+function setActive() { // Set Active
+  $('nav li [href="' + location.pathname.split("/")[1] + '"]').parent().addClass('active');
+};
+
+
