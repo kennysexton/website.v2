5ef9187d5cf2537b0d10e9e1034db44c8c8404e7
kennysexton <kennysexton4@gmail.com>
2021-02-26 10:17:09 -0500

    auto format

--- js/htmlReuse.js
+++ js/htmlReuse.js
@@ -1,10 +1,10 @@
-$(function(){
-	$('#footer').load('htmlSegments/footer.html');
+$(function () {
+  $('#footer').load('htmlSegments/footer.html');
 });
 
-$(function(){
-	$('nav').load('htmlSegments/navBar.html', function(){
-		setActive(); // Add active class to current tab
-	});
+$(function () {
+  $('nav').load('htmlSegments/navBar.html', function () {
+    setActive(); // Add active class to current tab
+  });
 
 });
--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,17 @@
 //Script for opening and closing menu
 function openNav() {
-  	document.getElementById("mySidebar").style.width = "55%";
+  document.getElementById("mySidebar").style.width = "55%";
 }
 function closeNav() {
-		document.getElementById("mySidebar").removeAttribute("style");
+  document.getElementById("mySidebar").removeAttribute("style");
 }
 
-$(window).on('resize',function(){  // detects change in width
-        closeNav();
+$(window).on('resize', function () {  // detects change in width
+  closeNav();
 });
 
 
-function setActive(){ // Set Active
+function setActive() { // Set Active
   $('nav li [href="' + location.pathname.split("/")[1] + '"]').parent().addClass('active');
 };
 
